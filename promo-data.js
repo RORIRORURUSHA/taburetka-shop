82730bc8b5962a069ae06af347461c49e87b9141
RORIRORURUSHA <d517nik@gmail.com>
2025-04-05 20:09:46 +0300

    Update promo-data.js

--- promo-data.js
+++ promo-data.js
@@ -1,13 +1,13 @@
 // Промокоды (название, скидка %, количество активаций)
 const promoCodes = [
-    { code: "TABURETKA5", discount: 5, activations: 100, used: 0 },
-    { code: "VAPE10", discount: 10, activations: 50, used: 0 },
-    { code: "SMOKE15", discount: 15, activations: 30, used: 0 },
-    { code: "POD20", discount: 20, activations: 20, used: 0 },
-    { code: "LIQUID25", discount: 25, activations: 15, used: 0 },
-    { code: "SHOP30", discount: 30, activations: 10, used: 0 },
-    { code: "BIGSALE40", discount: 40, activations: 5, used: 0 },
-    { code: "MEGASALE50", discount: 50, activations: 3, used: 0 }
+    { code: "TABURETKA5", discount: 5, activations: 0, used: 0 },
+    { code: "TABURETKAPIAR10", discount: 10, activations: 1, used: 0 },
+    { code: "SMOKE15", discount: 15, activations: 0, used: 0 },
+    { code: "POD20", discount: 20, activations: 0, used: 0 },
+    { code: "LIQUID25", discount: 25, activations: 0, used: 0 },
+    { code: "SHOP30", discount: 30, activations: 0, used: 0 },
+    { code: "BIGSALE40", discount: 40, activations: 0, used: 0 },
+    { code: "MEGASALE50", discount: 50, activations: 0, used: 0 }
 ];
 
 // Сертификаты (название, сумма, количество активаций, применяется к определенным товарам или при сумме)
@@ -18,15 +18,15 @@ const certificates = [
     { code: "LIQUID30", amount: 30, activations: 1, used: 0, applyTo: ["liquid1", "liquid2", "liquid3"] },
     
     // Сертификаты, которые применяются при достижении определенной суммы
-    { code: "BONUS200", amount: 200, activations: 1, used: 0, minOrderAmount: 1000 },
-    { code: "BONUS500", amount: 500, activations: 1, used: 0, minOrderAmount: 3000 },
-    { code: "BONUS1000", amount: 1000, activations: 1, used: 0, minOrderAmount: 5000 },
+    { code: "BONUS200", amount: 200, activations: 100, used: 0, minOrderAmount: 1000 },
+    { code: "BONUS500", amount: 500, activations: 100, used: 0, minOrderAmount: 3000 },
+    { code: "BONUS1000", amount: 1000, activations: 100, used: 0, minOrderAmount: 5000 },
     
     // Обычные сертификаты
     { code: "CERT100", amount: 100, activations: 1, used: 0 },
-    { code: "CERT200", amount: 200, activations: 1, used: 0 },
-    { code: "CERT300", amount: 300, activations: 1, used: 0 },
-    { code: "CERT400", amount: 400, activations: 1, used: 0 },
-    { code: "CERT500", amount: 500, activations: 1, used: 0 },
-    { code: "CERT1000", amount: 1000, activations: 1, used: 0 }
+    { code: "CERT200", amount: 200, activations: 0, used: 0 },
+    { code: "CERT300", amount: 300, activations: 0, used: 0 },
+    { code: "CERT400", amount: 400, activations: 0, used: 0 },
+    { code: "CERT500", amount: 500, activations: 0, used: 0 },
+    { code: "CERT1000", amount: 1000, activations: 0, used: 0 }
 ];
